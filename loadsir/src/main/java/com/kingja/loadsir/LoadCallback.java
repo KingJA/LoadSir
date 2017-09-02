534e7de212c9c8e312a04f1be4578129e11670c0
KingJA <kingjavip@gmail.com>
2017-09-02 17:26:38 +0800

    add README.md

---
+++ loadsir/src/main/java/com/kingja/loadsir/LoadCallback.java
@@ -0,0 +1,16 @@
+package com.kingja.loadsir;
+
+import android.view.View;
+
+/**
+ * Description:TODO
+ * Create Time:2017/9/2 17:04
+ * Author:KingJA
+ * Email:kingjavip@gmail.com
+ */
+public interface LoadCallback {
+    /**
+     * 创建自定义布局
+     */
+   public View onCreateLayout();
+}
